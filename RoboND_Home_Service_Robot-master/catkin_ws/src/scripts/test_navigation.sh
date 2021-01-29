410f83e324e1097df90642f8d9b59b5c95bdfb54
kvsaivineeth <skatasanivenkata@wpi.edu>
2021-01-28 21:56:54 -0500

    Remove copies

--- RoboND_Home_Service_Robot-master/catkin_ws/src/scripts/test_navigation.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-xterm -e "source devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(pwd)/src/world/Room_new.world" &
-sleep 5
-xterm -e "source devel/setup.bash; roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$(pwd)/src/maps/Room_map.yaml" &
-sleep 5
-xterm -e "source devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch" &
-
-
-
